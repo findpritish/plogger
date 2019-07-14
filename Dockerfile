202328b3cc05921b8fdda7c5a0b02941d0c3632f
Pritish <pritish@Pritishs-MacBook-Pro.local>
2019-07-14 20:51:00 +0530

    alpha push

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,23 @@
 FROM golang as builder
+LABEL maintainer="Pritish Payaningal <findpritish@gmail.com>"
+WORKDIR $GOPATH/src/github.com/findpritish/plogger
 RUN go get -d -v k8s.io/apimachinery/pkg/api/errors
+RUN go get -d -v k8s.io/apimachinery/pkg/labels
+RUN go get -d -v github.com/spf13/cobra
+RUN go get -d -v k8s.io/api/core/v1
+RUN go get -d -v github.com/fatih/color
+RUN go get -d -v k8s.io/client-go/kubernetes/typed/core/v1
+RUN go get -d -v k8s.io/apimachinery/pkg/apis/meta/v1
+RUN go get -d -v k8s.io/apimachinery/pkg/watch
+RUN go get -d -v github.com/pkg/errors
 RUN go get -d -v k8s.io/client-go/kubernetes
-RUN go get -d -v k8s.io/client-go/rest 
-RUN mkdir /build 
-COPY main.go /build/
-WORKDIR /build 
-RUN go build -o main .
+RUN go get -d -v k8s.io/client-go/rest
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /bin/plogger
 FROM alpine
-RUN adduser -S -D -H -h /app appuser
-USER appuser
-COPY --from=builder /build/main /app/
-WORKDIR /app
-CMD ["./main"]
+#RUN adduser -S -D -H -h /app appuser
+#USER appuser
+COPY --from=builder /bin/plogger .
+ENV NSINPUT "learning-ns"
+ENV EXINPUT "plogger"
+CMD ./plogger -n $NSINPUT --exclude-container $EXINPUT .
--- main.go
+++ main.go
@@ -1,29 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	//
-	// Uncomment to load all auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth
-	//
-	// Or uncomment to load specific auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+	"github.com/findpritish/plogger/cmd"
 )
 
 func main() {
-	podetails()
+	cmd.Run()
 }
 
-func podetails() {
+/*func podetails() {
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -34,8 +19,9 @@ func podetails() {
 	if err != nil {
 		panic(err.Error())
 	}
+
 	for {
-		pods, err := clientset.CoreV1().Pods("learningns").List(metav1.ListOptions{})
+		pods, err := clientset.CoreV1().Pods("learning-ns").List(metav1.ListOptions{})
 		if err != nil {
 			panic(err.Error())
 		}
@@ -44,7 +30,7 @@ func podetails() {
 		// Examples for error handling:
 		// - Use helper functions like e.g. errors.IsNotFound()
 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("learningns").Get("nginx1", metav1.GetOptions{})
+		_, err = clientset.CoreV1().Pods("learning-ns").Get("nginx", metav1.GetOptions{})
 		if errors.IsNotFound(err) {
 			fmt.Printf("Pod not found\n")
 		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
@@ -57,4 +43,4 @@ func podetails() {
 
 		time.Sleep(10 * time.Second)
 	}
-}
+}*/
